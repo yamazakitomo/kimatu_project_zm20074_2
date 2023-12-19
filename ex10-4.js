bd12684a44b17a7cbb8eadc880680b3e3905ffeb
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-12-19 04:38:52 +0000

    Update ex10-4.js

--- ex10-4.js
+++ ex10-4.js
@@ -1,5 +1,1 @@
-function nazo1(){if(document.getElementById('answer1').value =='バレンタイン'){alert('あたり！');}else if (document.getElementById('answer1').value == 'ばれんたいん') {
-        alert('あたり！');
-    } else {
-        alert('はずれ！');
-    }}
+function nazo1(){if(document.getElementById('answer1').value =='バレンタイン'){alert('あたり！');}else if (document.getElementById('answer1').value == 'ばれんたいん') {alert('あたり！');}else if (document.getElementById('answer1').value == 'バレンタインデー') {alert('あたり！');} else {alert('はずれ！');}}
