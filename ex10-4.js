5ed56c74683697648ec77ae57c8918f444efc610
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-12-19 13:07:22 +0900

    Create ex10-4.js

---
+++ ex10-4.js
@@ -0,0 +1,5 @@
+function nazo1(){if(document.getElementById('answer1').value =='バレンタイン'){alert('あたり！');}else if (document.getElementById('answer1').value == 'ばれんたいん') {
+        alert('あたり！');
+    } else {
+        alert('はずれ！');
+    }}
