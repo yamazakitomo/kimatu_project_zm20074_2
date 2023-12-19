da4586bed39a83206ebec53a017ab3e6ac4555cd
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-12-19 10:48:04 +0900

    Create ex10-2.js

---
+++ ex10-2.js
@@ -0,0 +1,5 @@
+function nazo(){if(document.getElementById('answer').value =='色'){alert('あたり！');}else if (document.getElementById('answer').value == 'いろ') {
+        alert('あたり！');
+    } else {
+        alert('はずれ！');
+    }}
