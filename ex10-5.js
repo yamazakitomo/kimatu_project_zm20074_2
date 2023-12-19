186bc32ecdf1fff0e3b2111e2f8abcb5abc2a614
yamazakitomo <131091953+yamazakitomo@users.noreply.github.com>
2023-12-19 13:33:20 +0900

    Update ex10-5.js

--- ex10-5.js
+++ ex10-5.js
@@ -1,4 +1,4 @@
-function nazo2(){if(Number(document.getElementById('answer2').value =='36')){alert('あたり！');}else if (document.getElementById('answer2').value == 'ばれんたいん') {
+function nazo2(){if(document.getElementById('answer2').value ==36){alert('あたり！');}else if (document.getElementById('answer2').value == '３６') {
         alert('あたり！');
     } else {
         alert('はずれ！');
